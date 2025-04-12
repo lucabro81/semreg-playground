db1e9454ed3773aae8689f94bf2a1c8b38481526
lucabro81 <luca.brognara81@gmail.com>
2025-04-12 15:56:26 +0200

    build: add assetPrefix, basePath

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   transpilePackages: ["@workspace/ui"],
+  assetPrefix: isProduction ? "/semreg-playground/" : undefined,
+  basePath: isProduction ? "/semreg-playground" : undefined,
 };
 
 export default nextConfig;
